19531550da9918daf3fd61b85c4f1203f8f3a5fa
luccaovf <lovfaria@sga.pucminas.br>
2023-08-30 19:00:38 -0300

    restante da atividade

--- TDD/Teste/testes/TesteRelogio.java
+++ TDD/Teste/testes/TesteRelogio.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
+
 import packMain.Relogio;
 
 class TesteRelogio {
@@ -15,5 +17,40 @@ class TesteRelogio {
         assertEquals(0, relogioTest.getMinuto());
         assertEquals(0, relogioTest.getSegundo());
 	}
+	@Test
+	void testProgramaHoraMinSeg() {
+		relogioTest.programaHoraMinSeg((byte)2, (byte)30, (byte)40);
+		assertEquals(2, relogioTest.getHora());
+        assertEquals(30, relogioTest.getMinuto());
+        assertEquals(40, relogioTest.getSegundo());
+	}
+	@Test
+	void testCronometro() {
+		int segPassados = relogioTest.cronometro((byte)2, (byte)30, (byte)40, (byte)4, (byte)33, (byte)55);
+		assertEquals(7395, segPassados);
+       
+	}
+	
 
+	@Test
+	void testImprimeHoraAM() {
+		relogioTest.programaHoraMinSeg((byte)20, (byte)30, (byte)40);
+		
+		assertEquals("08:30:40", relogioTest.imprimeHoraAM());
+	}
+	@Test
+	void testImprimeHora24h() {
+		relogioTest.programaHoraMinSeg((byte)20, (byte)30, (byte)40);
+		
+		assertEquals("20:30:40", relogioTest.imprimeHora24h());
+	}
+
+	@Test
+	void testAtualizaHora() {
+		relogioTest.atualizaHora();
+		//deu certo na hora
+		assertEquals(18, relogioTest.getHora());
+        assertEquals(57, relogioTest.getMinuto());
+        assertEquals(30, relogioTest.getSegundo());
+	}
 }
--- TDD/src/packMain/Relogio.java
+++ TDD/src/packMain/Relogio.java
@@ -1,4 +1,5 @@
 package packMain;
+import java.time.LocalTime;
 
 public class Relogio {
 	private byte hora;
@@ -31,5 +32,38 @@ public class Relogio {
 		this.hora= 00;
 		this.minuto= 00;
 		this.segundo=00;
+	}
+	
+	public void programaHoraMinSeg(byte horas, byte minutos, byte segundos) {
+        this.hora = horas;
+        this.minuto = minutos;
+        this.segundo = segundos;
+    }
+	
+	public int cronometro(byte horaInicio, byte minutoInicio, byte segundoInicio, byte horaFim, byte minutoFim, byte segundoFim) {
+		int segundosTotais;
+		//assumindo que pode se passar muito tempo mas nao mais que 1 dia e que o tempo final passado sempre ocorre depois do inicial
+		if(horaFim<horaInicio) {
+			horaFim += 24-horaInicio;
+		}
+		
+		segundosTotais= (horaFim*3600+minutoFim*60+segundoFim)-(horaInicio*3600+minutoInicio*60+segundoInicio);
+		return segundosTotais;
+	}
+	
+	public String imprimeHoraAM() {
+		byte horaEmAM = this.hora;
+		if(this.hora>12)
+			horaEmAM = (byte) (this.hora%12);
+		return String.format("%02d:%02d:%02d", horaEmAM, this.minuto, this.segundo);
+	}
+	
+	public String imprimeHora24h() {
+		return String.format("%02d:%02d:%02d", this.hora, this.minuto, this.segundo);
+	}
+	
+	public void atualizaHora() {
+		LocalTime horaAtual = LocalTime.now();
+		this.programaHoraMinSeg((byte)horaAtual.getHour() , (byte)horaAtual.getMinute(), (byte)horaAtual.getSecond());
 	}
 }
