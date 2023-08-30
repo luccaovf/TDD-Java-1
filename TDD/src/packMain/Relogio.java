f165ce46f9a0abb84b8b968da7c41dcb21639a7b
luccaovf <lovfaria@sga.pucminas.br>
2023-08-29 23:15:17 -0300

    getters e setters iniciais

--- TDD/src/packMain/Relogio.java
+++ TDD/src/packMain/Relogio.java
@@ -5,4 +5,25 @@ public class Relogio {
 	private byte minuto;
 	private byte segundo;
 	
+	public byte getHora() {
+		return hora;
+	}
+	public void setHora(byte hora) {
+		this.hora = hora;
+	}
+	
+	public byte getMinuto() {
+		return minuto;
+	}
+	public void setMinuto(byte minuto) {
+		this.minuto = minuto;
+	}
+	
+	public byte getSegundo() {
+		return segundo;
+	}
+	public void setSegundo(byte segundo) {
+		this.segundo = segundo;
+	}
+	
 }
